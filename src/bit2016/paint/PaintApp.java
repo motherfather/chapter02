9404512cdffd9b07a74b1c2888d6f5dd5547d570
motherfather <BITUSER@BITPC>
2016-09-20 09:34:07 +0900

    1111

--- src/bit2016/paint/PaintApp.java
+++ src/bit2016/paint/PaintApp.java
@@ -3,12 +3,12 @@ package bit2016.paint;
 public class PaintApp {
 
 	public static void main(String[] args) {
-		Point point = new Point();
+		Point point = new Point(); // 기본생성자
 		point.setX(100);
 		point.setY(1000);
 		point.show();
 		
-		Point point2 = new Point(123, 1234);
+		Point point2 = new Point(123, 1234); // 생성자
 		point2.show(true);
 		
 		point2.show(false);
